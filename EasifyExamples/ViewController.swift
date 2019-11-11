b0019b746030fd2af8629b2022c3c80c3448800d
Nabin Shrestha <nabin.shrestha@f1soft.com>
2019-11-11 13:37:28 +0545

    fixes minor issues for pod validation

--- EasifyExamples/ViewController.swift
+++ EasifyExamples/ViewController.swift
@@ -7,12 +7,13 @@
 //
 
 import UIKit
+import Easify
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
