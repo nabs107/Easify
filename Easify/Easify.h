1c190bcb00ed76b0da2eed7cbc5e548725227c38
Nabin Shrestha <nabin.shrestha@f1soft.com>
2019-11-11 12:35:08 +0545

    Initial Commit

---
+++ Easify/Easify.h
@@ -0,0 +1,19 @@
+//
+//  Easify.h
+//  Easify
+//
+//  Created by Nabin Shrestha on 11/11/19.
+//  Copyright © 2019 Personal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Easify.
+FOUNDATION_EXPORT double EasifyVersionNumber;
+
+//! Project version string for Easify.
+FOUNDATION_EXPORT const unsigned char EasifyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Easify/PublicHeader.h>
+
+
---
+++ EasifyExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EasifyExamples
+//
+//  Created by Nabin Shrestha on 11/11/19.
+//  Copyright © 2019 Personal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
